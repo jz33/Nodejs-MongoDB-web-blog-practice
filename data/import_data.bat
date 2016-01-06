01d9ffb3beb7cb2a868518d907afc359643156e1
Jun <junzhengrice@gmail.com>
2016-01-06 14:27:53 -0600

    Create import_data.bat

---
+++ data/import_data.bat
@@ -0,0 +1,4 @@
+REM Windows Batch
+mongoimport --db blog --collection posts data\posts01.json
+mongoimport --db blog --collection posts data\posts02.json
+mongoimport --db blog --collection posts data\posts03.json
